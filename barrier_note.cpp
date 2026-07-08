4604349239a9b696ee74f0f04ae82dc5a4d70f6e
realryo1 <realryo1@gmail.com>
2026-07-09 04:20:06 +0900

    Barrierモデル配置

--- barrier_note.cpp
+++ barrier_note.cpp
@@ -3,8 +3,9 @@
 
 void BarrierNote::Init(int lane, int face, float spawnZ, float speed)
 {
-	NoteBase::Init(lane, face, spawnZ, speed, "asset/model/cube.fbx");
+	NoteBase::Init(lane, face, spawnZ, speed, "asset/model/barrier.fbx");
+	SetSize(XMFLOAT3(1.3f, 1.3f, 1.3f));
 	m_ShaderType = S_LAMBERT;
-	SetColor(0.0f, 1.0f, 0.0f);
+	SetColor(1.0f, 1.0f, 1.0f);
 }
 
